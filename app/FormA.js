c77a9c37c16b98c6ae730f9005408d4b99040765
Subham Das <104473763+subham8391@users.noreply.github.com>
2023-09-15 20:54:30 +0530

    Update FormA.js

--- app/FormA.js
+++ app/FormA.js
@@ -1,31 +1,46 @@
-'use client'
-import { useState } from "react";
+'use client';
+import { useState } from 'react';
 
 function FormA({ onSubmit, age }) {
-    const handleSubmit = (event) => {
-    };
-    return (
-        <form id="dc" onSubmit={handleSubmit}>
-            <h2>Form A</h2>
-            <label>
-                Select DC Shows:
-                <select  >
-                    <option value="">--Select--</option>
-                    <option value="The Flash">The Flash</option>
-                    <option value="Arrow">Arrow</option>
-                    <option value="Supergirl">Supergirl</option>
-                    <option value="Legends of Tomorrow">Legends of Tomorrow</option>
-                </select>
-            </label>
-            <br />
-            <label>
-                Enter your age:
-                <input type="number" value={age} disabled />
-            </label>
-            <br />
-            <button id="submit-dc" type="submit">Submit</button>
+  const [dcShows, setDcShows] = useState('');
 
-        </form>
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit({
+      type: 'Form A',
+      age: age,
+      dcShows: dcShows,
+      marvelShows: null,
+    });
+  };
+
+  const handleChange = (event) => {
+    setDcShows(event.target.value);
+  };
+
+  return (
+    <form id='dc' onSubmit={handleSubmit}>
+      <h2>Form A</h2>
+      <label>
+        Select DC Shows:
+        <select value={dcShows} onChange={handleChange}>
+          <option value=''>--Select--</option>
+          <option value='The Flash'>The Flash</option>
+          <option value='Arrow'>Arrow</option>
+          <option value='Supergirl'>Supergirl</option>
+          <option value='Legends of Tomorrow'>Legends of Tomorrow</option>
+        </select>
+      </label>
+      <br />
+      <label>
+        Enter your age:
+        <input type='number' value={age} disabled />
+      </label>
+      <br />
+      <button id='submit-dc' type='submit'>
+        Submit
+      </button>
+    </form>
+  );
 }
 export default FormA;
